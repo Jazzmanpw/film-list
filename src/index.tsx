ff8e6fe9936a3950277d50d9f855a1f0fb42a2fd
Dmitry Shpak <jazzmanpw@mail.ru>
2021-01-05 11:15:26 +0300

    remove unused files from project initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { RecoilRoot } from 'recoil'
-import './index.css'
 import FilmInput from './film-input'
 import List from './list'
 
