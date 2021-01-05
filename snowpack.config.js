82bce83b87397d3546c6b873dcf745de3fea1501
Dmitry Shpak <jazzmanpw@mail.ru>
2021-01-05 10:42:49 +0300

    refine settings; add packages

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
@@ -16,7 +16,7 @@ module.exports = {
     /* ... */
   },
   devOptions: {
-    /* ... */
+    open: 'none',
   },
   buildOptions: {
     /* ... */
@@ -27,4 +27,4 @@ module.exports = {
   alias: {
     /* ... */
   },
-};
+}
