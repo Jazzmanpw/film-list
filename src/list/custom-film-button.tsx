cc6335ccca69a1f9e64c3ef4d92b6ef5fa06abbc
Dmitry Shpak <jazzmanpw@mail.ru>
2021-06-21 21:13:15 +0300

    custom-film-popup fix
    - close on submit
    - reset on close
    - make all the fields required

--- src/list/custom-film-button.tsx
+++ src/list/custom-film-button.tsx
@@ -5,17 +5,18 @@ import { useAddCustomFilm } from './atoms'
 import Film, { CustomFilm } from '../model/film'
 
 const customFilmAttributes = [
-  { key: 'nameRu' as const, text: 'Русское название' },
-  { key: 'nameEn' as const, text: 'Оригинальное название' },
-  { key: 'posterUrl' as const, text: 'Ссылка на постер' },
+  { key: 'nameRu' as const, text: 'Русское название', required: true },
+  { key: 'nameEn' as const, text: 'Оригинальное название', required: true },
+  { key: 'posterUrl' as const, text: 'Ссылка на постер', required: true },
   {
     key: 'posterUrlPreview' as const,
     text: 'Ссылка на маленькую версию постера',
+    required: true,
   },
 ]
 
 export default function CustomFilmButton() {
-  const { register, handleSubmit } = useForm<CustomFilm>()
+  const { handleSubmit, register, reset } = useForm<CustomFilm>()
   const addCustomFilm = useAddCustomFilm()
 
   const onSubmit: SubmitHandler<CustomFilm> = (customFilm) => {
@@ -27,32 +28,43 @@ export default function CustomFilmButton() {
       modal
       trigger={<button className="btn">Не нашли фильм?</button>}
       className="custom-film"
+      onClose={reset}
     >
-      <form
-        className="grid sm:p-2 sm:grid-cols-popup-content"
-        key="form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className="font-medium text-2xl" key="title">
-          Введите данные о фильме
-        </h1>
-        <ul className="sm:col-span-2 sm:row-start-2">
-          {customFilmAttributes.map(({ key, text }) => (
-            <li key={key}>
-              <label htmlFor={key}>{text}</label>
-              <input
-                className="input"
-                type="text"
-                required
-                {...register(key)}
-                autoComplete="off"
-                key={`${key}-input`}
-              />
+      {(close: () => void) => (
+        <form
+          className="grid sm:p-2 sm:grid-cols-popup-content"
+          key="form"
+          onSubmit={handleSubmit((film) => {
+            onSubmit(film)
+            close()
+          })}
+        >
+          <h1 className="font-medium text-2xl" key="title">
+            Введите данные о фильме
+          </h1>
+          <ul className="sm:col-span-2 sm:row-start-2">
+            {customFilmAttributes.map(({ key, text, required }) => (
+              <li key={key}>
+                <label htmlFor={key}>{text}</label>
+                <input
+                  className="input"
+                  type="text"
+                  required
+                  {...register(key, { required })}
+                  autoComplete="off"
+                  key={`${key}-input`}
+                />
+              </li>
+            ))}
+            <li key="seen">
+              <label htmlFor="seen">
+                <input type="checkbox" {...register('seen')} /> Уже видел
+              </label>
             </li>
-          ))}
-        </ul>
-        <button className="btn block mt-2 w-full sm:m-0">Готово</button>
-      </form>
+          </ul>
+          <button className="btn block mt-2 w-full sm:m-0">Готово</button>
+        </form>
+      )}
     </Popup>
   )
 }
