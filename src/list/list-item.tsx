e4dbf4d71b307eb523396d269422cd4d69bdfbf2
Dmitry Shpak <jazzmanpw@mail.ru>
2021-06-27 23:34:29 +0300

    finer "film seen" toggle

--- src/list/list-item.tsx
+++ src/list/list-item.tsx
@@ -1,9 +1,9 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { assoc, prop } from 'ramda'
-import React, { PropsWithChildren, useMemo } from 'react'
+import { always, lensProp, not, over } from 'ramda'
+import React from 'react'
 import type { FilmData } from '../model/film'
-import SwitchButton from '../switch-button'
+import SwitchToggle from '../switch-toggle'
 import { joinTruthy } from '../utils'
 import { useEditFilm, useRemoveFilm } from './atoms'
 
@@ -11,74 +11,44 @@ type Props = {
   film: FilmData
 }
 
+const getSeenLabel = (seen?: boolean) => (seen ? 'Уже смотрел' : 'К просмотру')
+
 export default function ListItem({ film }: Props) {
   const remove = useRemoveFilm()
-  const setSeen = useEditFilm<boolean>(assoc('seen'), film.filmId)
-  const seenSwitchData = useMemo(
-    () => [
-      {
-        children: 'К просмотру',
-        active: !film.seen,
-        onClick: () => setSeen(false),
-      },
-      {
-        children: 'Уже видел',
-        active: film.seen,
-        onClick: () => setSeen(true),
-      },
-    ],
-    [film.seen],
+  const toggleSeen = useEditFilm<boolean>(
+    always(over(lensProp('seen'), not)),
+    film.filmId,
   )
 
   return (
-    <li
-      className="group relative flex gap-4 lg:overflow-auto"
-      key={film.filmId}
-    >
-      <img
-        className="w-1/4 object-cover object-center sm:w-1/6 md:w-1/4 xl:w-1/6 2xl:w-1/4"
-        src={film.posterUrlPreview}
-        alt={joinTruthy([film.nameRu, film.nameEn && `(${film.nameEn})`])}
-      />
-      <div className="flex-1">
-        <div className="font-semibold pr-4">{film.nameRu}</div>
-        <div className="text-gray-500">{film.nameEn}</div>
-        <SwitchButton
-          propsArr={seenSwitchData}
-          childComponent={Button}
-          createKey={prop('children')}
+    <li>
+      <article
+        className="group relative flex gap-4 lg:overflow-auto"
+        key={film.filmId}
+      >
+        <img
+          className="w-1/4 object-cover object-center sm:w-1/6 md:w-1/4 xl:w-1/6 2xl:w-1/4"
+          src={film.posterUrlPreview}
+          alt={joinTruthy([film.nameRu, film.nameEn && `(${film.nameEn})`])}
         />
-        <FontAwesomeIcon
-          icon={faTimes}
-          className="absolute top-1 right-1 cursor-pointer hover:text-red-600 lg:text-red-500 lg:opacity-0 lg:group-hover:opacity-100"
-          onClick={() => remove(film)}
-        />
-      </div>
+        <div className="flex-1">
+          <h1 className="font-semibold pr-4">{film.nameRu}</h1>
+          <h2 className="text-gray-500">{film.nameEn}</h2>
+          <SwitchToggle
+            label={getSeenLabel}
+            checked={film.seen}
+            onChange={toggleSeen}
+            className="h-6"
+            id={`film-${film.filmId}`}
+            key={film.filmId}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className="absolute top-1 right-1 cursor-pointer hover:text-red-600 lg:text-red-500 lg:opacity-0 lg:group-hover:opacity-100"
+            onClick={() => remove(film)}
+          />
+        </div>
+      </article>
     </li>
-  )
-}
-
-type ButtonProps = {
-  activeClassName: string
-  active?: boolean
-  className: string
-  onClick?: () => void
-}
-
-function Button({
-  activeClassName,
-  active,
-  className,
-  onClick,
-  children,
-}: PropsWithChildren<ButtonProps>) {
-  return (
-    <button
-      role="button"
-      className={joinTruthy([className, active && activeClassName])}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   )
 }
---
+++ src/switch-toggle.tsx
@@ -0,0 +1,53 @@
+import React, { ChangeEvent } from 'react'
+import { joinTruthy } from './utils'
+
+type Props = {
+  checked?: boolean
+  className?: string
+  id: string
+  label: string | ((checked?: boolean) => string)
+  onChange: (checked: boolean) => void
+}
+
+export default function SwitchToggle({
+  checked,
+  className,
+  id,
+  label,
+  onChange,
+}: Props) {
+  const onChangeInternal = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.checked)
+  }
+
+  return (
+    <label
+      htmlFor={id}
+      className={`${joinTruthy([
+        'flex items-center cursor-pointer',
+        className,
+      ])}`}
+    >
+      <div className="relative">
+        <input
+          type="checkbox"
+          id={id}
+          className="sr-only"
+          checked={checked}
+          onChange={onChangeInternal}
+        />
+        <div className="block bg-red-100 w-7 h-4 rounded-full" />
+        <div
+          className={joinTruthy([
+            `absolute left-0.5 top-0.5 bg-white w-3 h-3 rounded-full transition`,
+            !checked && 'bg-red-300',
+            checked && 'bg-red-600 transform translate-x-full',
+          ])}
+        />
+      </div>
+      <div className="ml-1 text-gray-700 text-sm font-medium">
+        {typeof label === 'string' ? label : label(checked)}
+      </div>
+    </label>
+  )
+}
