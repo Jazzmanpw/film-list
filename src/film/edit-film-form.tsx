ace8271f53d1ff35178a0370f3570ffe6582f22d
Dmitry Shpak <jazzmanpw@mail.ru>
2021-08-15 22:08:07 +0300

    extra fields for EditFilmForm

--- src/film/edit-film-form.tsx
+++ src/film/edit-film-form.tsx
@@ -1,14 +1,23 @@
 import { Field, Form, Formik } from 'formik'
-import React from 'react'
+import { not } from 'ramda'
+import React, { ComponentType, useState } from 'react'
 import Button from '../button'
 import Input from '../input'
-import type { EditableFilmData } from './model'
+import type { EditableFilmData, FilmData } from './model'
 
-const attrs = {
-  name: 'Русское название',
-  originalName: 'Оригинальное название',
-  thumbnailUrl: 'Ссылка на маленькую версию постера',
-}
+type AttributeBase = { field: keyof FilmData; text: string; required?: boolean }
+type Attribute = AttributeBase & { component?: ComponentType<AttributeBase> }
+
+const mainAttributes: Attribute[] = [
+  { field: 'name', text: 'Русское название' },
+  { field: 'originalName', text: 'Оригинальное название' },
+  { field: 'thumbnailUrl', text: 'Ссылка на маленькую версию постера' },
+]
+
+const extraAttributes: Attribute[] = [
+  { field: 'href', text: 'Ссылка' },
+  { field: 'year', text: 'Год' },
+]
 
 type Props = {
   film: EditableFilmData
@@ -17,6 +26,8 @@ type Props = {
 }
 
 export default function EditFilmForm({ film, onCancel, onSubmit }: Props) {
+  const [expanded, setExpanded] = useState(false)
+
   return (
     <Formik<EditableFilmData> initialValues={film} onSubmit={onSubmit}>
       <Form className="grid sm:p-2 md:grid-cols-[1fr,auto]">
@@ -24,27 +35,32 @@ export default function EditFilmForm({ film, onCancel, onSubmit }: Props) {
           Введите данные о фильме
         </h1>
         <ul className="md:col-span-2 md:row-start-2">
-          {Object.entries(attrs).map(([key, text]) => (
-            <li key={key}>
-              <label htmlFor={key} key={`${key}-label`}>
-                {text}
-              </label>
-              <Field
-                id={key}
-                as={Input}
-                name={key}
-                type="text"
-                required
-                autoComplete="off"
-                key="name-input"
-              />
-            </li>
-          ))}
-          <li key="seen">
-            <label htmlFor="seen">
+          {mainAttributes.map(
+            ({ component: Component = TextField, ...props }) => (
+              <li key={props.field}>
+                <Component {...props} required />
+              </li>
+            ),
+          )}
+          <li key="seen" className="flex">
+            <label htmlFor="seen" className="flex-1">
               <Field id="seen" type="checkbox" name="seen" /> Уже видел
             </label>
+            <button
+              className="underline text-blue-500"
+              onClick={() => setExpanded(not)}
+              type="button"
+            >{`Показать ${expanded ? 'меньше' : 'больше'}`}</button>
           </li>
+          {expanded
+            ? extraAttributes.map(
+                ({ component: Component = TextField, ...props }) => (
+                  <li key={props.field}>
+                    <Component {...props} required />
+                  </li>
+                ),
+              )
+            : null}
         </ul>
         <section className="flex gap-2 mt-2 w-full md:m-0">
           <Button className="flex-1" secondary onClick={onCancel}>
@@ -56,5 +72,21 @@ export default function EditFilmForm({ film, onCancel, onSubmit }: Props) {
         </section>
       </Form>
     </Formik>
+  )
+}
+
+function TextField({ field, required, text }: AttributeBase) {
+  return (
+    <>
+      <label htmlFor={field}>{text}</label>
+      <Field
+        id={field}
+        as={Input}
+        name={field}
+        type="text"
+        required={required}
+        autoComplete="off"
+      />
+    </>
   )
 }
